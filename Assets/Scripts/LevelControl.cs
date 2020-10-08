222c947873b602ed87059fb580515001b64ed0ab
mmumpton <maureen.mumpton@techngs.com>
2020-10-08 12:40:10 -0400

    initial commit

---
+++ Assets/Scripts/LevelControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour
+{
+    public int index;
+    public string levelName;
+    
+    void onTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Loading Level with build index
+            //SceneManager.LoadScene(index);
+
+            // Loading level with scene name
+            //SceneManager.LoadScene("levelName");
+
+            //Restart lvl
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
